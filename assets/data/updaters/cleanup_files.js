2f118be43db31e0ff5c22c0335c9e10e39bb52f4
akupiec <kupiecart@gmail.com>
2022-06-11 21:10:16 +0200

    cleanup_files

--- assets/data/updaters/cleanup_files.js
+++ assets/data/updaters/cleanup_files.js
@@ -9,27 +9,31 @@ const nodes = parse(fileData, {
   skip_empty_lines: true,
 });
 
-// function removeUnusedNodes() {
-// const fileData2 = fs.readFileSync(path.join(__dirname, "../connections.csv"));
-// const links = parse(fileData2, {
-//   columns: true,
-//   skip_empty_lines: true
-// });
-//
-// const sources = [...new Set(links.map(l => [l.Source_1, l.Source_2, l.Source_3]).flat())].filter(a => !!a)
-// const targets = [...new Set(links.map(l => l.Target))].filter(a => !!a)
-//
-// const toKeep = nodes.filter(n => sources.includes(n.Name) || targets.includes(n.Name));
-// const out = stringify(toKeep, {
-//   header: true,
-// })
-//
-// fs.writeFileSync(path.join(__dirname, "../nodes_.csv"), out);
-// }
+function removeUnusedNodes() {
+  const fileData2 = fs.readFileSync(path.join(__dirname, "../connections.csv"));
+  const links = parse(fileData2, {
+    columns: true,
+    skip_empty_lines: true,
+  });
 
-// const fileNames = nodes.map(n => n.Image)
-// console.log(fileNames);
-//
-// fileNames.forEach(n => {
-//   fs.copyFileSync(path.join(__dirname, '..', '..', n), path.join(__dirname, '..','..', 'out', n))
-// })
+  const sources = [...new Set(links.map((l) => [l.Source_1, l.Source_2, l.Source_3]).flat())].filter((a) => !!a);
+  const targets = [...new Set(links.map((l) => l.Target))].filter((a) => !!a);
+
+  const toKeep = nodes.filter((n) => sources.includes(n.Name) || targets.includes(n.Name));
+  const out = stringify(toKeep, {
+    header: true,
+  });
+
+  fs.writeFileSync(path.join(__dirname, "../nodes_.csv"), out);
+}
+
+function removeUneusedImages() {
+  const fileNames = nodes.map((n) => n.Image);
+  console.log(fileNames);
+
+  fileNames.forEach((n) => {
+    fs.copyFileSync(path.join(__dirname, "..", "..", n), path.join(__dirname, "..", "..", "out", n));
+  });
+}
+
+// removeUneusedImages();
